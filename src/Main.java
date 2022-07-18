094bdba959ee1ed4d47f04d215b1d0e59d5ccd3a
zerihun-CS <98953012+zerihun-CS@users.noreply.github.com>
2022-07-18 16:17:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//option 1
+        int numberz[] = {6,3,1,-8,20,-26,3,8};
+
+        int upCount = 5;
+        int partialSum = 0;
+        int perviousSum = 0;
+        boolean flag =true;
+        int count = 0;
+        int numbers[] = {2,3,1,-6,8,-3,-1,3};
+        for (int i = 0; i < numberz.length ; i++) {
+            partialSum = partialSum + numberz[i];
+            System.out.println(partialSum);
+            if (partialSum > upCount && perviousSum <=upCount) {
+
+                count++;
+
+
+            }
+            perviousSum = partialSum;
+        }
+        System.out.println(count);
+
+//option 2
+
+        for (int i = 0; i < numberz.length ; i++) {
+            partialSum = partialSum + numberz[i];
+            if (partialSum > upCount && flag == true){
+                count++;
+                flag =false;
+            } else if (partialSum < upCount) {
+                flag =true;
+
+            }
+        }
+
     }
+
+
 }
